b6a82438be99f2e9912de642831d1f27409dd4a7
cuiyaxuan <35548337+cuiyaxuan@users.noreply.github.com>
2025-11-17 02:48:04 +0900

    Rename project from DenoiseST to scSTADE

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'DenoiseST'
+project = 'scSTADE'
 copyright = '2024, Cui Yaxuan'
 author = 'Cui Yaxuan'
 release = '1'
